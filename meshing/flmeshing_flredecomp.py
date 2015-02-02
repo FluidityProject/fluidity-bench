738dacf23d928819a13ff441fc5079b13d324831
Michael Lange <michael.lange@imperial.ac.uk>
2015-02-02 12:52:41 +0100

    FLMeshing: Adding fluidity run to flredecomp benchmark

--- meshing/flmeshing_flredecomp.py
+++ meshing/flmeshing_flredecomp.py
@@ -4,9 +4,12 @@ import subprocess
 import re
 
 regions = ['flredecomp', 'flredecomp::gmsh_read', 'flredecomp::state', 'flredecomp::zoltan']
+regions += ['fluidity', 'fluidity::gmsh_read', 'fluidity::state']
 
 re_flredecomp = re.compile(r"\/flredecomp\s+::\s+([0-9\.]+)")
+re_fluidity = re.compile(r"\/fluidity\s+::\s+([0-9\.]+)")
 re_io_gmsh = re.compile(r"I\/O\s+::\s+Gmsh\s+::\s+([0-9\.]+)")
+re_io_state = re.compile(r"I\/O\s+::\s+Populate\_State\s+::\s+([0-9\.]+)")
 re_flrd_state = re.compile(r"\/flredecomp\s+::\s+Populate State\s+::\s+([0-9\.]+)")
 re_flrd_zoltan = re.compile(r"\/flredecomp\s+::\s+zoltan\_drive\s+::\s+([0-9\.]+)")
 
@@ -22,6 +25,7 @@ class FLRedecompMeshing(FLMeshing):
         self.create_flml(dim, size)
 
         self.run_flredecomp(dim, size, nprocs)
+        self.run_fluidity(dim, size, nprocs)
 
     def run_flredecomp(self, dim, size, nprocs):
         infile = self.filename(name='sim', dim=dim, size=size, end='')
@@ -44,6 +48,26 @@ class FLRedecompMeshing(FLMeshing):
 
         except subprocess.CalledProcessError as e:
             print "Warning: flredecomp failed with size %f" % size
+
+    def run_fluidity(self, dim, size, nprocs):
+        flmlfile = self.filename(name='sim', dim=dim, size=size, end='_flrd.flml')
+        # flmlfile = fname.substitute(dir=meshdir[dim], name='sim', dim=dim, lf='%.4f'%self._lf, end='_flrd.flml')
+
+        cmd = ['mpiexec', '-n', '%d'%nprocs]
+        cmd += [os.environ['FLUIDITY_DIR'] + "/bin/fluidity"]
+        cmd += ['-l', '-p', flmlfile]
+        try:
+            # Execute flredecomp in the generated .flml and mesh files
+            subprocess.check_call(cmd)
+
+            # Read performance result from log file
+            with file("fluidity.log-0", "r") as lf:
+                log = lf.read()
+                self.register_timing("fluidity", float(re_fluidity.search(log).group(1)))
+                self.register_timing("fluidity::state", float(re_io_state.search(log).group(1)))
+                self.register_timing("fluidity::gmsh_read", float(re_io_gmsh.search(log).group(1)))
+        except subprocess.CalledProcessError as e:
+            print "Warning: fluidity failed with size %f" % size
 
 if __name__ == '__main__':
     # Benchmark
